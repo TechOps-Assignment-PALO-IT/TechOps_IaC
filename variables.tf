953c60d1665a14760b10e581c8cef7442c0c5c4d
Russell John Gapasin <rjharvest@hotmail.com>
2023-01-16 02:21:01 +0800

    feat: Add Github variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "github_token" {
+  description = "The Github token of the user or organization"
+}
+
+variable "github_owner" {
+  description = "The GitHub owner or organization name"
+  default = "TechOps-Assignment-PALO-IT"
+}
+
+variable "github_repos" {
+  description = "A list of Github repository names"
+  type        = list(string)
+  default     = ["TechOps_Frontend_Test", "TechOps_Backend_Test"]
+}
