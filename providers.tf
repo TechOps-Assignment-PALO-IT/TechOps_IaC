aa9867b84476d1c3a1033478303c6a2629a1e0e6
Russell John Gapasin <rjharvest@hotmail.com>
2023-01-16 02:20:31 +0800

    feat: Add Github provider

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
