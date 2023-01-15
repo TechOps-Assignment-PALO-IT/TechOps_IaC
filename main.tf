6a762c8a5158452603a3f1d6dca67d75c9d44535
Russell John Gapasin <rjharvest@hotmail.com>
2023-01-16 02:21:21 +0800

    feat: Add Github resource

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "github_repository" "github_repos" {
+  count      = length(var.github_repos)
+  name       = var.github_repos[count.index]
+  visibility = "public"
+  auto_init  = true
+}
+
+resource "github_branch" "development" {
+  count      = length(var.github_repos)
+  repository = github_repository.github_repos[count.index].name
+  branch     = "dev"
+}
+
+resource "github_branch_default" "default" {
+  count      = length(var.github_repos)
+  repository = github_repository.github_repos[count.index].name
+  branch     = github_branch.development[count.index].branch
+}
+
+resource "github_branch_protection_v3" "branch_protection" {
+  count      = length(var.github_repos)
+  repository = github_repository.github_repos[count.index].name
+  branch     = "main"
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+  }
+}
